23d5db13939d9289a1046f7d69e117e98a009ff5
Ege Cimsir <cimsir@imfusion.com>
2026-06-04 20:39:36 +0200

    Standalone Nova3R model base. (WIP)

--- nova3r/__init__.py
+++ nova3r/__init__.py
@@ -1,28 +1,29 @@
 # Copyright (c) 2026 Weirong Chen
 """NOVA3R: Non-pixel-aligned Visual Transformer for Amodal 3D Reconstruction."""
 
-from nova3r.models import Nova3rImgCond, Nova3rPtsCond, BatchModelWrapper
 from nova3r.inference import inference_nova3r
 from nova3r.io import (
     load_images,
     make_pairs,
     save_pointcloud_ply,
     load_model,
-    predict,
 )
 from nova3r.utils.device import get_default_device, resolve_device, autocast
 from nova3r.scripts.download_checkpoints import download_checkpoints
+from nova3r.model import Nova3r, predict, preprocess
+
 
 __all__ = [
-    "Nova3rImgCond",
-    "Nova3rPtsCond",
-    "BatchModelWrapper",
+    # Base
+    "Nova3r",
+    "predict", 
+    "preprocess",
+    # Functions
     "inference_nova3r",
     "load_images",
     "make_pairs",
     "save_pointcloud_ply",
     "load_model",
-    "predict",
     "get_default_device",
     "resolve_device",
     "autocast",
